d7d9cd001ac84cefea08e23dc6d0fbc044a47ec5
Simone Goti <gz7pytfpdb@privaterelay.appleid.com>
2026-02-05 16:53:27 +0100

    esercizio impossibile

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -4,6 +4,31 @@ import Form from "react-bootstrap/Form";
 import { FloatingLabel } from "react-bootstrap";
 
 class AddComment extends Component {
+  state = {
+    comment: "",
+    rate: 1,
+    elementId: this.props.asin,
+  };
+
+  AddComment() {
+    const fetchURL = `https://striveschool-api.herokuapp.com/api/comments/${this.props.asin}`;
+
+
+    const commentData = {
+        comment : this.state.comment,
+        rate: this.state.rate,
+        elementId:this.props.asin
+    }
+  }
+
+  fetch(fetchURL,){
+    method:"POST",
+    headers:{
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTczM2Q0Njg1ZTNiMTAwMTViNWVkOTAiLCJpYXQiOjE3NzAzMDMyOTYsImV4cCI6MTc3MTUxMjg5Nn0.J_uMfQej1JyBhgkd_AVpMD9K7vyOqzIbX-ddI3JhOLc",
+            body: JSON.stringify(commentData)
+  }
+  }
+
   render() {
     return (
       <>
--- src/components/CommentArea.jsx
+++ src/components/CommentArea.jsx
@@ -3,11 +3,35 @@ import CommentList from "./CommentList";
 import AddComment from "./AddComment";
 
 class CommentArea extends Component {
-  state = {};
+  state = { comments: [] };
+  GetComments = () => {
+    const fetchURL = `https://striveschool-api.herokuapp.com/api/comments/${this.props.asin}`;
+
+    fetch(fetchURL, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTczM2Q0Njg1ZTNiMTAwMTViNWVkOTAiLCJpYXQiOjE3NzAzMDMyOTYsImV4cCI6MTc3MTUxMjg5Nn0.J_uMfQej1JyBhgkd_AVpMD9K7vyOqzIbX-ddI3JhOLc",
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error("errore");
+        }
+      })
+      .then((data) => {
+        this.setState({ comments: data });
+      })
+      .catch((err) => console.log(err + " stai sbagliando"));
+  };
+  componentDidMount() {
+    this.GetComments();
+  }
   render() {
     return (
       <>
-        {/* <CommentList list={this.state.comments} /> */}
+        <CommentList list={this.state.comments} />
 
         <AddComment />
       </>
